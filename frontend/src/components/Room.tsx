e7a8d42f9be6d6620be8883e82784c79766fc27e
Official-Krish <krishanand0103@gmail.com>
2024-06-03 16:14:40 +0530

    WebRTC

--- frontend/src/components/Room.tsx
+++ frontend/src/components/Room.tsx
@@ -1,15 +1,85 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useSearchParams } from "react-router-dom"
+import { Socket, io } from "socket.io-client";
 
+const URL = "http://localhost:3000";
 export const RoomComp = () => {
     const [searcParams, setSearchParams] = useSearchParams();
     const name = searcParams.get("name");
+    const [lobby, setLobby] = useState(true);
+    const [socket, setSocket] = useState<null | Socket>(null);
+    const [sendingPc, setSendingPc] = useState<null | RTCPeerConnection>(null);
+    const [receivingPc, setReceivingPc] = useState<null | RTCPeerConnection>(null);
+    const [remoteVideoTrack, setRemoteVideoTrack] = useState<MediaStreamTrack | null>(null);
+    const [localVideoTracck, setlocalVideoTracck] = useState<MediaStreamTrack, null>(null);
+    const [remoteAudioTrack, setRemoteAudioTrack] = useState<MediaStreamTrack | null>(null);
+    const [localAudioTrack, setLocalAudioTrack] = useState<MediaStreamTrack, null>(null);
 
-    useEffect(()=>{
-    },[]);
+
+    useEffect( () => {
+        const socket = io(URL)
+        socket.on('send-offer', async ({roomId}) => {
+            setLobby(false);
+            const pc = new RTCPeerConnection();
+            setSendingPc(pc);
+            const sdp = await pc.createOffer();
+            socket.emit ("offer", {
+                sdp,
+                roomId,
+            })
+        })
+        socket.on("offer", async ({roomId,offer}) => {
+            setLobby(false);
+            const pc = new RTCPeerConnection();
+            pc.setRemoteDescription({
+                sdp : offer,
+                type : "offer"
+            })
+            const sdp = await pc.createAnswer();
+            setReceivingPc(pc);
+
+            pc.ontrack = (({track,type}) => {
+                if (type == "audio"){
+                    setRemoteAudioTrack(track);
+                }else {
+                    setRemoteAudioTrack(track);
+                }
+            })
+            socket.emit ("answer", {
+                roomId,
+                sdp : ""
+            })
+        })
+        socket.on("asnwer", ({roomId, answer} ) => {
+            setLobby(false);
+            setSendingPc(pc => {
+                pc?.setRemoteDescription({
+                    type: "answer",
+                    sdp: answer
+                })
+                return pc;
+            })
+        })
+
+        socket.on("lobby",  () => {
+            setLobby(true);
+        });
+        
+
+        setSocket(socket);
+    },[name]);
+
+
+    if(lobby){
+        return <div>
+            Waiting to connect you to someone
+        </div>
+    }
 
     
     return <div>
-        Room
+        Hi {name}
+        <video width={400} height={400} />
+        <video width={400} height={400} />
     </div>
 }
